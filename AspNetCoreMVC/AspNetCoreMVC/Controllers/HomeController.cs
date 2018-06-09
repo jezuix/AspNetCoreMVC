34bf6852d701ee790ca89bc457f3d2f0dbe3e527
Rodolpho Rangel de Barros <mr_condor_@hotmail.com>
2018-06-09 17:04:51 -0300

    Projeto base de Asp.net Core MVC

---
+++ AspNetCoreMVC/AspNetCoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        [HttpGet]
+        public IActionResult Index(HomeModel formulario)
+        {
+            CarregarListaDados();
+            return View();
+        }
+
+        public void CarregarListaDados()
+        {
+            HomeModel objHomeModel = new HomeModel();
+
+            ViewBag.Lista = objHomeModel.GetAll();
+        }
+    }
+}
---
+++ AspNetCoreMVC/AspNetCoreMVC/Models/HomeModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMVC.Models
+{
+    public class HomeModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public List<HomeModel> GetAll()
+        {
+            HomeModel item;
+            List<HomeModel> lista = new List<HomeModel>();
+
+            item = new HomeModel();
+            item.Id = 1;
+            item.Nome = "Rodolpho";
+            lista.Add(item);
+
+            item = new HomeModel();
+            item.Id = 2;
+            item.Nome = "Jacinto";
+            lista.Add(item);
+
+            return lista;
+        }
+    }
+}
